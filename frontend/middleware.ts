e526e6feeb318acb424c252f3cc828fd117561d7
wr1159 <chuweiron18@gmail.com>
2025-05-31 13:33:42 +0200

    Remove vercel middelware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,9 +1,8 @@
-import { updateSession } from "@/lib/supabase/middleware";
-import { type NextRequest } from "next/server";
+// import { updateSession } from "@/lib/supabase/middleware";
+// import { type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
-}
+// export async function middleware(request: NextRequest) { // return await updateSession(request);
+// }
 
 export const config = {
   matcher: [
